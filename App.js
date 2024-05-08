f6dc5f8abbccc1d4c036a94e2a1c53c6483cf125
Marcos Leal <marcosantoniocordeiro27@gmail.com>
2024-05-08 10:54:16 -0300

    up

--- App.js
+++ App.js
@@ -1,21 +1,73 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
 
-export default function App() {
+// Importa o básico para o código funcionar
+import { View, TextInput, Text, TouchableOpacity } from "react-native";
+
+// Requisições HTTP
+import axios from "axios";
+
+// Importa a estilizaçao
+import { styles } from "./src/styles/Style";
+
+// Armazena as informações inseridas
+const App = () => {
+  const [cep, setCep] = useState("");
+  const [address, setAddress] = useState(null);
+
+  // Definir a função, o async indica que esta função vai lidar com operações assíncronas.
+  const fetchAddress = async () => {
+    // O código será executado, se algo der errado,será lidado no catch
+    try {
+      // Solicitação HTTP. O await pausa a execução até que a solicitação seja concluída.
+      const response = await axios.get(`https://viacep.com.br/ws/${cep}/json/`);
+      // O resultado é armazenado na variável response.
+      setAddress(response.data);
+    } catch (error) {
+      // Variável que contém informações se teve erro.
+      console.error("Error fetching address:", error);
+      setAddress(null);
+    }
+  };
+
   return (
+    // Parte visual do código
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.digite}>
+        {/* Input para preencher e armazenar o cep inserido */}
+        <TextInput
+          placeholder="Digite o CEP"
+          value={cep}
+          onChangeText={setCep}
+          keyboardType="numeric"
+          
+        />
+
+        {/* Botão que inicializa a funçao fetchAdress */}
+        <TouchableOpacity style={styles.touch} onPress={fetchAddress}>
+          {/* Estilização do botão */}
+          <Text
+            style={{ fontSize: 25, backgroundColor: "red", color: "white", widht: 10, height: 25,  }}
+          >
+            Encontrar
+          </Text>
+        </TouchableOpacity>
+        </View>
+      
+
+      {/* Após o botão ser clicado, retorna os resultados da função fetcAddress */}
+      {address && (
+
+
+        <View style={styles.resultados}>
+          <Text>CEP: {address.cep}</Text>
+          <Text>Rua: {address.logradouro}</Text>
+          <Text>Bairro: {address.bairro}</Text>
+          <Text>Cidade: {address.localidade}</Text>
+          <Text>Estado: {address.uf}</Text>
+        </View>
+      )}
     </View>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
---
+++ src/styles/Style.js
@@ -0,0 +1,31 @@
+import { StyleSheet } from "react-native";
+// Estilizações
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "blue",
+    
+
+  },
+
+  digite: {
+    flex: 1,
+    backgroundColor: "green",
+    alignItems: "center",
+    justifyContent: "center",
+    width: "30%",
+    height: "30%",
+  },
+
+  resultados: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    
+    width: "30%",
+    height: "30%",
+  },
+
+  
+});
